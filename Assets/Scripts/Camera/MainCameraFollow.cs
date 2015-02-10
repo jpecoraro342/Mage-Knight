5ced43480bf052447c45206ba83721a9fb94b7f0
jpecoraro342 <jpecoraro342@ufl.edu>
2015-02-10 13:11:39 -0500

    Added a camera follow script

---
+++ Assets/Scripts/Camera/MainCameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using System.Collections;
+
+public class MainCameraFollow : MonoBehaviour 
+{
+	public Transform target;
+	public float smoothing = 5f;
+	
+	Vector3 offset;
+	
+	void Start() 
+	{
+		//Creates initial offset
+		offset = transform.position - target.position;
+	}
+	
+	//We used fixedupdate to move the camera since we are following a physics object
+	void FixedUpdate() 
+	{
+		Vector3 targetCamPos = target.position + offset;
+		
+		//Lerp is a smoothing movement
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
