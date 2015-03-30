52008f60248ad33c401aa6c545c08d2a3f2edb64
jpecoraro342 <jpecoraro342@ufl.edu>
2015-03-29 20:55:00 -0400

    Player Deals Damage with sword, added magic spell assets

--- Assets/Scripts/Character/PlayerAttacking.cs
+++ Assets/Scripts/Character/PlayerAttacking.cs
@@ -15,31 +15,55 @@ public class PlayerAttacking : MonoBehaviour {
 
 	Animator animator;
 
+	public CapsuleCollider swordCollider;
+
+	float Attack1Time = .6f;
+
 	static string Attack1 = "Attack1";
 	static string Attack2 = "Attack2";
 	static string Attack3 = "Attack3";
 	static string Attack4 = "Attack4";
 
-	static string AlternateAttac = "AltAttack";
-
-	bool isAttacking = false;
+	static string AlternateAttack = "AltAttack";
 	
+	bool isAttacking = false;
+	bool canAttack = true;
+
+	bool altAttackPressed = false;
+
 	void Awake () {
 		animator = GetComponent<Animator>();
 	}
 
 	void Update () {
-		if (Input.GetButton(Attack1) && !isAttacking) {
+		if (Input.GetButton(AlternateAttack) && !altAttackPressed) {
+
+		}
+		else if (!Input.GetButton(AlternateAttack)) {
+
+		}
+
+
+		if (Input.GetButton(Attack1) && !isAttacking && canAttack) {
 			Debug.Log("Attack!");
 			isAttacking = true;
 			animator.SetTrigger(Attack1);
-			StartCoroutine(StartAttacking(.6f));
+			StartCoroutine(StartAttacking(Attack1Time));
 		}
+		else if (!Input.GetButton(Attack1)) {
+			canAttack = true;
+		}
+	}
+
+	public void MeleeTrigger(GameObject enemy) {
+		Debug.Log("Deal Damage");
 	}
 
 	IEnumerator StartAttacking(float attackTime) {
 		yield return null;
+		swordCollider.enabled = true;
 		yield return new WaitForSeconds(attackTime);
+		swordCollider.enabled = false;
 		isAttacking = false;
 	}
 }
