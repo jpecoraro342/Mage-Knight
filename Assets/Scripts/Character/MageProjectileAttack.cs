108f60941fad915b9ca63b1a5079d8b819812c5d
jpecoraro342 <jpecoraro342@ufl.edu>
2015-04-02 18:04:05 -0400

    Added basic mage spell with targeting system

---
+++ Assets/Scripts/Character/MageProjectileAttack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MageProjectileAttack : MonoBehaviour {
+
+	public float speed = 2f;
+	public float projectileLife = 4f;
+
+	void Start () {
+		StartCoroutine(DelayDestroy(projectileLife));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward.normalized * speed * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Enemy") {
+			Debug.Log("Deal Damage");
+			//Start a fire particle system on the enemy to show damage dealt to them. 
+			//Deal the damage
+
+			//Destroy or keep going?
+			//StartCoroutine(DelayDestroy(.2f));
+		}
+	}
+
+	IEnumerator DelayDestroy(float time) {
+		yield return null;
+		yield return new WaitForSeconds(time);
+		
+		Destroy(gameObject);
+	}
+}
--- Assets/Scripts/Character/PlayerAttacking.cs
+++ Assets/Scripts/Character/PlayerAttacking.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /*
  * This class will handle all of the attacking for the player
@@ -16,20 +17,19 @@ public class PlayerAttacking : MonoBehaviour {
 	Animator animator;
 
 	public CapsuleCollider swordCollider;
+	public GameObject mageAttack1Clone;
 
-	float Attack1Time = .8f;
-	float Attack1AnimationTime = 1.05f;
+	LinkedList<GameObject> enemyTargetList;
 
-	float MageAttack1Time = .8f;
-	float MageAttack1AnimationTime = 1f;
-
+	//Button Mappings
 	static string Attack1 = "Attack1";
 	static string Attack2 = "Attack2";
 	static string Attack3 = "Attack3";
 	static string Attack4 = "Attack4";
 
 	static string AlternateAttack = "AltAttack";
-	
+
+	//Attack Bools
 	bool isAttacking = false;
 	bool canAttack = true;
 
@@ -38,12 +38,24 @@ public class PlayerAttacking : MonoBehaviour {
 	bool meleeAttacksActive = true;
 	bool mageAttacksActive = false;
 
+	//Attack Times Note: These really shouldnt be static..
+	float mageAttack1Distance = 20f;
+	
+	float Attack1Time = .8f;
+	float Attack1AnimationTime = 1.05f;
+	
+	float MageAttack1Time = .8f;
+	float MageAttack1AnimationTime = 1f;
+
 	int BaseLayerIndex = 0;
 	int MeleeAttackLayerIndex = 1;
 	int MageAttackLayerIndex = 2;
 
+
+
 	void Awake () {
 		animator = GetComponent<Animator>();
+		enemyTargetList = new LinkedList<GameObject>();
 	}
 
 	void Update () {
@@ -79,6 +91,47 @@ public class PlayerAttacking : MonoBehaviour {
 		Debug.Log("Deal Damage");
 	}
 
+	public void OnTriggerEnter(Collider other) {
+		GameObject triggerObject = other.gameObject;
+		if (triggerObject.tag == "Enemy") {
+			Debug.Log("Enter");
+			enemyTargetList.AddLast(other.gameObject);
+		}
+	}
+
+	public void OnTriggerExit(Collider other) {
+		GameObject triggerObject = other.gameObject;
+		if (triggerObject.tag == "Enemy") {
+			Debug.Log("Exit");
+			enemyTargetList.Remove(other.gameObject);
+		}
+	}
+
+	Vector3 getTargetTransformDirection() {
+		Vector3 closest = transform.forward;
+		float closestDistance = float.MaxValue;
+
+		foreach (GameObject enemy in enemyTargetList) {
+			Vector3 enemyPosition = enemy.transform.position;
+			float distance = Vector3.Distance(transform.position, enemyPosition);
+			if (distance < closestDistance) {
+				closest = enemyPosition;
+				closestDistance = distance;
+			}
+		}
+
+		//We did not change, just use forward position
+		if (closestDistance != float.MaxValue) {
+			float oldY = closest.y;
+			closest = closest - transform.position;
+			closest.y = oldY + .5f;
+		}
+
+		return closest;
+	}
+
+	//Attack Enumerators
+
 	IEnumerator StartMeleeAttack(float attackTime, float animationTime) {
 		yield return null;
 		animator.SetLayerWeight(MeleeAttackLayerIndex, 1);
@@ -95,7 +148,36 @@ public class PlayerAttacking : MonoBehaviour {
 	IEnumerator StartMageAttack1() {
 		yield return null;
 
+		RaycastHit hit;
+		Vector3 attackTransform = transform.position;
+		bool targetFound = false;
+
+
+//		int xoffset = -2;
+//		for (int i = 0; i < 5; i++) {
+//			Vector3 raycastVect = transform.position;
+//			raycastVect.x += xoffset;
+//			if (Physics.Raycast(raycastVect, transform.forward, out hit, mageAttack1Distance)) {
+//				Debug.DrawRay(transform.position, hit.point, Color.green, 2);
+//				attackTransform = hit.point;
+//				break;
+//			}
+//		}
+
+		attackTransform.y = transform.position.y + .5f;
+
+		GameObject attack = (GameObject)Instantiate (mageAttack1Clone, attackTransform, Quaternion.identity);
+		attack.transform.parent = gameObject.transform.parent;
+
+		Vector3 Direction = getTargetTransformDirection();
+		Debug.Log(transform.forward +  ": " + Direction);
+		attack.transform.forward = Direction;
+
+
 		yield return new WaitForSeconds(MageAttack1Time);
+
+		//This is handled in the objects class itself
+		//Destroy(attack.gameObject);
 
 		isAttacking = false;
 	}
