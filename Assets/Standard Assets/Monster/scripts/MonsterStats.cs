3a6959a6bd5875a7c91a965c90595888af01ad08
cdolbier <c.dolbier@ufl.edu>
2015-04-16 20:33:25 -0400

    changed lighting. torches working. imported standard assets

---
+++ Assets/Standard Assets/Monster/scripts/MonsterStats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterStats : MonoBehaviour {
+	public int maxHealth;
+	public int currentHealth;
+	public int monsterDamage;
+	public bool isDead;
+	public float attackSpeed;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		maxHealth = 50;
+		currentHealth = maxHealth;
+		monsterDamage = 10;
+		isDead = false;
+		attackSpeed = 1f;
+		speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
