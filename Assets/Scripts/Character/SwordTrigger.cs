3e15553917d63016b042bf3c2f72901359f234c4
Alex-Coleman <alex.coleman93@gmail.com>
2015-04-19 13:22:52 -0400

    Main Menu Added
    Added new Title Screen, with logo, credits screen, and script to have
    the camera rotate. All of this is in a new scene.

---
+++ Assets/Scripts/Character/SwordTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordTrigger : MonoBehaviour {
+
+	static string EnemyTag = "Enemy";
+	static string BossTag = "Boss";
+
+	public PlayerAttacking playerAttackScript;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == EnemyTag || other.gameObject.tag == BossTag) {
+			playerAttackScript.MeleeTrigger(other.gameObject);
+		}
+	}
+}
