d21f0a962a102dd26dedbd0b5d12acaf0e509757
Chris James <tobaljackson@gmail.com>
2015-04-19 23:39:53 -0400

    added summoner, fixed monster death attacking

---
+++ Assets/Scripts/Enemy/GrollScripts/MonsterHealth.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterHealth : MonoBehaviour {
+	GameObject player;
+	Animator animator;
+	NavMeshAgent agent;
+	MonsterStats stats;
+	public Transform healthOrb;
+	private float deathTime;
+
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		stats = GetComponent<MonsterStats> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		animator = GetComponent<Animator> ();
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stats.currentHealth <= 0) {
+			agent.enabled = false;
+			if (!stats.isDead){
+				animator.SetTrigger("dead");
+				stats.isDead = true;
+				deathTime = Time.time;
+				//if (Random.value < 0.75){
+				Instantiate(healthOrb, this.gameObject.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+				//}
+			}
+			if (stats.isDead && Time.time - deathTime > 5) {
+				this.transform.root.Translate (new Vector3(0,-0.1f,0) * Time.deltaTime);
+			}
+			
+			if (stats.isDead && Time.time - deathTime > 13) {
+				DestroyObject (this.gameObject);
+			}
+		}
+	}
+
+	public void TakeDamage(float damage) {
+		stats.currentHealth -= damage;
+	}
+}
---
+++ Assets/Scripts/Enemy/GrollScripts/MonsterStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterStats : MonoBehaviour {
+	public float maxHealth;
+	public float currentHealth;
+	public int monsterDamage;
+	public bool isDead;
+	public float attackSpeed;
+	public float speed;
+	public float visionCone;
+	public float visionRadius;
+	public bool seenPlayer;
+	public float timeSinceSeenPlayer;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
